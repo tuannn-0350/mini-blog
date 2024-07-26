535eec774fc4553a04c6c185326248bfb745de17
tuanne <nguyen.ngoc.tuan-c@sun-asterisk.com>
2024-07-26 10:35:25 +0700

    init db

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  has_secure_password
+  has_many :posts, dependent: :destroy
+
+  has_many :active_relationships, class_name: Relationship.name,
+foreign_key: :follower_id, dependent: :destroy
+  has_many :passive_relationships, class_name: Relationship.name,
+foreign_key: :followed_id, dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  has_many :reactions, dependent: :destroy
+  has_many :reacted_posts, through: :reactions, source: :post
+end
